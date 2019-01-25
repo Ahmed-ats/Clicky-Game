ddc1197293bb86510e8bb4a0c40d19dcae1ee52d
Ahmed <ahmd.h.ats@gmail.com>
2019-01-24 19:33:16 -0800

    Updates

---
+++ precache-manifest.ae0a9c0777dcb78a1684256cdebc8c59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "bf2e8f546176644c97ed",
+    "url": "/clicky-game/static/js/main.bf2e8f54.chunk.js"
+  },
+  {
+    "revision": "77c432302e3bd7e0ef20",
+    "url": "/clicky-game/static/js/1.77c43230.chunk.js"
+  },
+  {
+    "revision": "bf2e8f546176644c97ed",
+    "url": "/clicky-game/static/css/main.b0cb3332.chunk.css"
+  },
+  {
+    "revision": "a0ba57f64512f10c8813ac3ed99f54f7",
+    "url": "/clicky-game/index.html"
+  }
+];
